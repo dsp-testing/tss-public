1b1401ac6a7e227a0e8eaf4ee5c8c7a26339b3a4
Kevin Sawicki <kevinsawicki@gmail.com>
2020-07-30 11:28:07 -0700

    Update amazon.cpp

--- amazon.cpp
+++ amazon.cpp
@@ -1,2 +1,2 @@
 export const aws_access_key_id = "AKIAXYZDQCENQJEKE6U2";
-export const aws_secret_access_key = "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"
+export const aws_secret_access_key = "EXHrK5rin23Et68qjJAhp7gKMMUaqd61B0zuyXrV"
